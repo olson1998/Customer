406d05867b50084c9164c413fe9595aad109a9ca
MSI <aleksander.inglot98@gmail.com>
2022-02-11 22:55:00 +0100

    update

--- src/main/java/com/customer/app/CustomerController.java
+++ src/main/java/com/customer/app/CustomerController.java
@@ -1,10 +1,11 @@
 package com.customer.app;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.time.Clock;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,15 +20,21 @@ public class CustomerController {
     @GetMapping
     @RequestMapping("/all")
     public List<Customer> allCustomers(){
-        System.out.println("Returned all at " + LocalDateTime.now());
+        System.out.println("Returned all at: " + getCurrentTime()); //wyświetlenie w konsoli informacji
         return cs.getAllCustomers();
     }
 
     @GetMapping
     @RequestMapping("/search/{pesel}")
     public Optional<Customer> searchCustomer(@PathVariable String pesel){
-        System.out.println("Returned" + cs.searchForCustomerByPesel(pesel).toString() + LocalDateTime.now());
+        //wyświetlenie w konsoli informacji
+        System.out.println("Returned" + cs.searchForCustomerByPesel(pesel).toString() + " at: "  +getCurrentTime());
         return cs.searchForCustomerByPesel(pesel);
+    }
+
+    //zwraca czas lokalny
+    private LocalDateTime getCurrentTime(){
+        return LocalDateTime.now().withNano(0);
     }
 
 }
